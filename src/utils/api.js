4204f75b1eee56f131fbefdc1cdfd0c786d5f331
sumitjoshiakit-web <sumitjoshiakit@gmail.com>
2026-08-24 19:15:14 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,14 @@
-// API utility functions can be added here.
+const API_BASE = 'https://dummyjson.com/products';
 
-export default {};
+export async function fetchAllProducts() {
+  const res = await fetch(API_BASE);
+  if (!res.ok) throw new Error('Failed to fetch products');
+  const data = await res.json();
+  return data.products;
+}
+
+export async function fetchProductById(id) {
+  const res = await fetch(`${API_BASE}/${id}`);
+  if (!res.ok) throw new Error(`Failed to fetch product #${id}`);
+  return res.json();
+}
